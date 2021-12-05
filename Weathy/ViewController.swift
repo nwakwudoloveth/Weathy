63893dafe0753911cb6f73de3bf95a521d3ba4d7
loveth <nwakwudoloveth@gmail.com>
2021-12-05 20:26:09 +0100

    Initial Commit

---
+++ Weathy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weathy
+//
+//  Created by Loveth Nwakwudo on 12/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

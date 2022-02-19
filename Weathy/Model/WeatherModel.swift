a37c5870f576df621ae187e211a59bf1a55bef12
loveth <nwakwudoloveth@gmail.com>
2022-02-19 11:24:28 +0100

    Weathy

--- Weathy/Model/WeatherModel.swift
+++ Weathy/Model/WeatherModel.swift
@@ -12,7 +12,7 @@ struct WeatherModel {
     let temperature: Double
     let descripsion: String
     var temperatureString: String {
-        return String(format: "%.1f", temperature)
+        return String(format: "%.0f", temperature)
     }
     
     var conditionName: String {
